aa519d3978e94cbb572d80ba83cfd0798cb67b7c
Peter <p.armytank@gmail.com>
2016-08-20 20:31:52 +0000

    add ajax query for stocks

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,9 +1,10 @@
-var getQuote= function(symbol){
-    var url = "https://www.quandl.com/api/v3/datasets/WIKI/" + symbol + ".json";
+var getQuote= function(stock, callback){
+    var url = "https://www.quandl.com/api/v3/datasets/WIKI/" + stock.symbol + ".json?api_key=" + 'VsizqnSyP2XPZeWZDnwb';
+    console.log(url);
     $.ajax({
         url:url,
         success: function(data) {
-         console.log(data);
+         callback(data.dataset);
         },
         error: function() {
          console.log("error");
--- scripts/react.jsx
+++ scripts/react.jsx
@@ -1,28 +1,35 @@
 
 var StockList = React.createClass({
     getInitialState: function(){
-        var symbols = this.props.symbols;
-        return {symbols:symbols};
+        var stocks = this.props.stocks;
+        return {stocks:stocks};
     },
     inputSymbol: function(event){
         this.setState({newSymbol: event.target.value});
     },
     addNewSymbol: function(){
         console.log('clicked');
-        this.setState({symbols: this.state.symbols.concat([this.state.newSymbol])});
-        console.log(this.state.newSymbol);
-        console.log(this.state.symbols);
+        var newStock = {'symbol': this.state.newSymbol}
+        this.setState({stocks: this.state.stocks.concat([newStock])});
+    },
+    update: function(){
+        this.state.stocks.forEach(function(stock){
+            console.log(stock);
+            getQuote(stock, function(data){
+                stock["name"] = data.name;
+            })
+        })
     },
     render: function(){
-        var name="Stocks"
-        var symbols = this.state.symbols;
+        var stocks = this.state.stocks;
         return (
             <div>
                 <input value={this.state.newSymbol} onChange={this.inputSymbol} />
                 <button onClick={this.addNewSymbol}>Add</button>
+                <button onClick={this.update}>Update</button>
                 <ul>
-                    {symbols.map(function(symbol){
-                        return <li>{symbol}</li>
+                    {stocks.map(function(stock){
+                        return <StockBox stock={stock} />
                     })}
                 </ul>
             </div>
@@ -30,6 +37,21 @@ var StockList = React.createClass({
     }
 })
 
+var StockBox = React.createClass({
+    getInitialState: function(){
+        var symbol = this.props.stock.symbol;
+        console.log(symbol);
+        var name = this.props.stock.name;
+        console.log(name);
+        return {symbol:symbol, name:name}
+    },
+    render: function(){
+        return (
+            <h1>{this.state.symbol}</h1>
+            )
+    }
+})
+
 var destination = document.querySelector("#container");
 
-ReactDOM.render(<StockList symbols={["MSFT","AAPL"]} />, destination);
+ReactDOM.render(<StockList stocks={[{symbol:"MSFT"},{symbol:"AAPL"}]} />, destination);
